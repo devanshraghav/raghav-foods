587e2ff63f9b0ec8e8bfde1f0e73c2ea331e46f3
Devansh Raghuvanshi <64578916+devanshraghav@users.noreply.github.com>
2025-06-30 17:55:33 +0530

    Update in MenuItem.js file

--- src/components/Menu/MenuItem.js
+++ src/components/Menu/MenuItem.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ItemQuantityButton from "./ItemQuantityButton";
+import { Image_CDN_Url } from "../../constants";
 
 const MenuItem = ({ data }) => {
   return (
@@ -7,7 +8,9 @@ const MenuItem = ({ data }) => {
       <div className="flex justify-between py-4 border-b-2">
         <div>
           <h1 className="text-base font-bold text-gray-700">{data.name}</h1>
-          <h1 className="text-sm">₹{data.price / 100}</h1>
+          <h1 className="text-sm">
+            ₹{(data?.price ? data.price : data.defaultPrice) / 100}
+          </h1>
           <h1 className="w-full lg:w-4/5 text-sm my-4 text-gray-400">
             {data.description}
           </h1>
@@ -17,12 +20,9 @@ const MenuItem = ({ data }) => {
             <div className="w-32">
               <img
                 className="w-full rounded-md"
-                src={
-                  "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/" +
-                  data.imageId
-                }
+                src={Image_CDN_Url + data.imageId}
               />
-              <ItemQuantityButton data={data}/>
+              <ItemQuantityButton data={data} />
             </div>
           )}
         </div>
